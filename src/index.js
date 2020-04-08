3b8bc6e217a7107fe48710c636956d7f72a8c5e6
Elvira Ramírez <elvirarp92@gmail.com>
2020-04-08 22:54:42 +0200

    start screen

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-app.init()
+const startButton = document.getElementById("start-button")
+const startScreen = document.getElementById("start-screen")
+const gameApp = document.getElementById("game-app")
+
+startButton.addEventListener("click", event => {
+  startScreen.style.display = "none"
+  gameApp.style.display = "block"
+  app.init()
+})
