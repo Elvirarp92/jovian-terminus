4f0e776f396713cca1dafdc7f5da08bcda3e60f7
Elvira Ramírez <elvirarp92@gmail.com>
2020-04-09 17:36:34 +0200

    add way to configure tile transitability manually

--- src/map.js
+++ src/map.js
@@ -1,10 +1,11 @@
 class Map {
-  constructor(rows, cols, tileArray) {
+  constructor(rows, cols, tileArray, upperArray) {
     this.dimensions = { rows: rows, cols: cols }
     this.tileSize = 48
     this.tileset = new Image()
     this.tileset.src = './img/scifi_space_rpg_tiles.png'
     this.array = tileArray
+    this.upperLayer = upperArray
     this.tilesPerRow = 8 //number of tiles per row in our tileset pic
   }
 
@@ -19,32 +20,43 @@ class Map {
 }
 
 class Tile {
-  constructor(id) {
+  constructor(id, transitability) {
     this.id = id
-    if (
-      id == 62 ||
-      id == 63 ||
-      id == 64 ||
-      id == 70 ||
-      id == 71 ||
-      id == 72 ||
-      id == 78 ||
-      id == 79 ||
-      id == 80 ||
-      id == 83 ||
-      id == 84 ||
-      id == 85 ||
-      id == 86 ||
-      id == 87 ||
-      id == 88 ||
-      id == 94 ||
-      id == 95 ||
-      id == 96 ||
-      id == 101
-    ) {
-      this.isTransitable = true
-    } else {
-      this.isTransitable = false
+
+    switch(transitability) {
+      case true: 
+       this.isTransitable = true
+       break
+      case false:
+        this.isTransitable = false
+        break
+      default:
+        if (
+          id == 62 ||
+          id == 63 ||
+          id == 64 ||
+          id == 70 ||
+          id == 71 ||
+          id == 72 ||
+          id == 78 ||
+          id == 79 ||
+          id == 80 ||
+          id == 83 ||
+          id == 84 ||
+          id == 85 ||
+          id == 86 ||
+          id == 87 ||
+          id == 88 ||
+          id == 94 ||
+          id == 95 ||
+          id == 96 ||
+          id == 101
+        ) {
+          this.isTransitable = true
+        } else {
+          this.isTransitable = false
+        }
+        break
     }
   }
 }
